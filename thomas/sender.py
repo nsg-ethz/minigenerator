1afd7f2a7114734e22f30bdb901558e5d4610411
Thomas Holterbach <thomasholterbach@gmail.com>
2018-05-17 10:51:42 +0100

    active flows flow rate is random between 0.05s and 0.4s.

--- thomas/sender.py
+++ thomas/sender.py
@@ -27,7 +27,7 @@ for flow_id in range(0, 100):
     flow_template = {"dst": '8.0.0.2',
                      "dport": 5000+flow_id,
                      "sport": 6000+flow_id,
-                     "inter_packet_delay":0.2,
+                     "inter_packet_delay":uniform(0.05, 0.4),
                      "duration": 60}
 
     print flow_template
