c32fe2007cc9a081077f8dddaeae792e9ba4f6c1
Thomas Holterbach <thomasholterbach@gmail.com>
2018-07-26 17:48:01 +0100

    trying 1ms delay

--- minigenerator/flowlib/tcp_thomas.py
+++ minigenerator/flowlib/tcp_thomas.py
@@ -77,9 +77,9 @@ def recvFlowTCP(dport=5001,**kwargs):
     print 'run: iptables -F'
     os.system('iptables -F')
 
-    print 'run: tc qdisc add dev ens3 root netem delay 200ms'
+    print 'run: tc qdisc add dev ens3 root netem delay 1ms'
     os.system('tc qdisc del dev ens3 root netem')
-    os.system('tc qdisc add dev ens3 root netem delay 200ms')
+    os.system('tc qdisc add dev ens3 root netem delay 1ms')
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
