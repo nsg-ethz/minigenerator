4e319358406aa1cec0bf4302a03a0367974ba824
Thomas Holterbach <thomasholterbach@gmail.com>
2018-07-26 17:28:18 +0100

    pkt_len=1500 after 30s

--- minigenerator/flowlib/tcp_thomas.py
+++ minigenerator/flowlib/tcp_thomas.py
@@ -76,7 +76,9 @@ def recvFlowTCP(dport=5001,**kwargs):
     connection_ts = None
     print 'run: iptables -F'
     os.system('iptables -F')
+
     print 'run: tc qdisc add dev ens3 root netem delay 200ms'
+    os.system('tc qdisc del dev ens3 root netem')
     os.system('tc qdisc add dev ens3 root netem delay 200ms')
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
